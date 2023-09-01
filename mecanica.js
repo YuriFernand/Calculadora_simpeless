346ce1c44d286a05bb75a7e8d254fea9b7ce62fe
YuriFernand <fernandobarradossantosyuri@gmail.com>
2023-09-01 18:34:39 -0300

    selecionando elementos

---
+++ mecanica.js
@@ -0,0 +1,11 @@
+//
+//d
+//
+
+// Seleção  dos elementos 
+
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botaoNumero = document.querySelectorAll(".num");
+const botaoOperadores = document.querySelectorAll(".operador")
